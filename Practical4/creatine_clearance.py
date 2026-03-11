2cd5f6b14c45c89a3e3ea87dec10e171e83cc3d2
shuhan <shuhan.25@intl.zju.edu.cn>
2026-03-11 13:46:12 +0800

    Complete Practical4: all tasks finished

--- Practical4/creatine_clearance.py
+++ Practical4/creatine_clearance.py
@@ -15,38 +15,41 @@
 #5. Display the result
 
 #Get user inputs
-age=float(input("Enter age (years):"))
-weight=float(input("Enter weight (kg):"))
-gender=input("Enter gender (male/female):").lower() #in small letter
-Cr=float(input("Enter creatinine concentration (umol/L):"))
+
 
 valid_input = True
-
+age=float(input("Enter age (years):"))
 #Validate age
 if age >= 100:
     print("ERROR: Age must be less than 100 years")
-    valid_input = False
-
+    print("Please correct the errors above and run the program again.")
+    exit()
+    
+weight=float(input("Enter weight (kg):"))
 #Validate weight
 if weight <= 20 or weight >= 80:
     print("ERROR: Weight must be between 20 kg and 80 kg")
-    valid_input = False
-
+    print("Please correct the errors above and run the program again.")
+    exit()
+    
+Cr=float(input("Enter creatinine concentration (umol/L):"))
 #Validate creatinine concentration
 if Cr <= 0 or Cr >= 100:
     print("ERROR: Creatinine concentration must be between 0 and 100 umol/L")
-    valid_input = False
-
+    print("Please correct the errors above and run the program again.")
+    exit()
+    
+gender=input("Enter gender (male/female):").lower() #in small letter
 #Validate gender
 if gender != "male" and gender != "female":
     print("ERROR: Gender must be either 'male' or 'female'")
-    valid_input = False 
+    print("Please correct the errors above and run the program again.")
+    exit()
+
 
 #Calculate CrCl if all inputs are valid
-if valid_input:
-    CrCl=((140-age)*weight)/(72*Cr)
-    if gender=="female":
-        CrCl=CrCl*0.85
-    print("creatine clearance rate:",CrCl)
-else:
-    print("Please correct the errors above and run the program again.")
+
+CrCl=((140-age)*weight)/(72*Cr)
+if gender=="female":
+    CrCl=CrCl*0.85
+print("creatine clearance rate:",CrCl)
